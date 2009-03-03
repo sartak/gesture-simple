3956ff6f46da00b1c5fb76edc81fce6c39baa0d6
Shawn M Moore <sartak@gmail.com>
2009-03-03 02:25:05 -0500

    Force add_template arguments to be Template objects $recognizer->match

--- lib/Gesture/Simple.pm
+++ lib/Gesture/Simple.pm
@@ -1,15 +1,32 @@
 package Gesture::Simple;
 use Any::Moose;
 
+use Gesture::Simple::Template;
+
 has templates => (
-    is      => 'ro',
-    isa     => 'ArrayRef',
-    default => sub { [] },
+    is         => 'ro',
+    isa        => 'ArrayRef',
+    default    => sub { [] },
+    auto_deref => 1,
 );
 
 sub add_template {
     my $self = shift;
+
+    for (@_) {
+        blessed($_) && $_->isa('Gesture::Simple::Template')
+            or confess "$_ is not a Gesture::Simple::Template.";
+    }
+
     push @{ $self->templates }, @_;
+}
+
+sub match {
+    my $self    = shift;
+    my $gesture = shift;
+
+    my @matches = map { $_->match($gesture) } $self->templates;
+    return sort { $b->score <=> $a->score } @matches;
 }
 
 __PACKAGE__->meta->make_immutable;
