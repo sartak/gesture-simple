086402653c067298dd0f92d51d70d2fce59b4878
Shawn M Moore <sartak@gmail.com>
2009-03-03 02:24:40 -0500

    Add Gesture::Simple::Template

---
+++ lib/Gesture/Simple/Template.pm
@@ -0,0 +1,35 @@
+package Gesture::Simple::Template;
+use Any::Moose;
+
+use Gesture::Simple::Match;
+
+use constant match_class => 'Gesture::Simple::Match';
+
+has name => (
+    is       => 'ro',
+    isa      => 'Str',
+    required => 1,
+);
+
+has points => (
+    is       => 'ro',
+    isa      => 'ArrayRef',
+    required => 1,
+);
+
+sub match {
+    my $self    = shift;
+    my $gesture = shift;
+
+    return $self->match_class->new(
+        template => $template,
+        gesture  => $gesture,
+        score    => $score,
+    );
+}
+
+__PACKAGE__->meta->make_immutable;
+no Any::Moose;
+
+1;
+
